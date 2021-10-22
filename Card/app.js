05e8a971658d0e6f4e5d97a5bfb6bd6d13ffdb46
mari293 <maricruzg020903@gmail.com>
2021-10-21 21:31:22 -0500

    Se termina de dar los estilos a la card que se genera, además se crean nuevos elementos en html

--- Card/app.js
+++ Card/app.js
@@ -5,17 +5,18 @@ const informationContainer = document.querySelector("#container-information");
 const cardContainer = document.querySelector("#container-card");
 const card = document.querySelector("#card");
 const nameCard = document.querySelector("#name-card");
+const nameCardcontainer = document.querySelector("#container-name-card");
 const messageWelcome = document.querySelector("#welcome");
 const nameUser = document.querySelector("#name");
 const message = document.querySelector("#message");
 const btn_Generate = document.querySelector("#generate");
 
-nameUser.addEventListener('keydown', characterValidation)
-btn_Generate.addEventListener('focus', createCard)
+nameUser.addEventListener('keydown', characterValidation);
+btn_Generate.addEventListener('focus', createCard);
 
 function characterValidation (keyboardEvent) {
     const code   = keyboardEvent.keyCode;
-    btn_Generate.style.backgroundColor = "#ccc";
+    btn_Generate.style.backgroundColor = "#e5e5e5";
     if(code==55 || code==187 || code==189){
         message.style.color = "#d00000";
         message.textContent = "No puede contener los siguientes carácter '*' , '+' , '/' , '-' , '_'";
@@ -27,10 +28,12 @@ function characterValidation (keyboardEvent) {
 };
 
 function createCard(){
-    btn_Generate.style.backgroundColor = "#0077b6";
+    btn_Generate.style.backgroundColor = "#90e0ef";
     nameCard.textContent = `${nameUser.value}`;
+    nameCard.style.fontSize = "120%";
+    nameCard.style.fontWeight = "bold";
     messageWelcome.textContent = "Bienvenido al sistema";
-    card.style.height = "90px";
+    card.style.height = "450px";
     card.style.width = "85%";
     card.style.border = "1px solid #000";
     card.style.boxShadow = "0px 0px 6px 1px red";
@@ -38,7 +41,14 @@ function createCard(){
     card.style.display = "flex";
     card.style.flexDirection = "column";
     card.style.alignItems = "center";
-    card.style.justifyContent = "center";
+    card.style.justifyContent = "space-evenly";
+    nameCardcontainer.style.boxShadow = "inset 0px 20px 10px  #d9d9d9";
+    nameCardcontainer.style.height= "250px";
+    nameCardcontainer.style.width= "50%";
+    nameCardcontainer.style.borderRadius = "50%";
+    nameCardcontainer.style.display = "flex";
+    nameCardcontainer.style.alignItems = "center";
+    nameCardcontainer.style.justifyContent = "center";
 }
 
 title.style.color = "#ae2012";
@@ -64,11 +74,12 @@ userContainer.style.alignItems= "center";
 userContainer.style.alignItems= "center";
 userContainer.style.width = "35%";
 userContainer.style.justifyContent = "space-evenly";
+userContainer.style.boxShadow = "0px 0px 6px 1px #ccc";
 
 btn_Generate.style.height = "50px";
 btn_Generate.style.fontWeight = "800";
 btn_Generate.style.borderRadius= "8px";
-btn_Generate.style.backgroundColor = "#ccc";
+btn_Generate.style.backgroundColor = "#e5e5e5";
 
 informationContainer.style.border = "1px solid #000";
 informationContainer.style.width = "50%";
