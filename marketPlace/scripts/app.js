c1c5a8da158912347a102cfb7e831e7c92adb316
mari293 <maricruzg020903@gmail.com>
2021-11-06 12:21:43 -0500

    Se soluciona error de eliminar un producto en el carrito

--- marketPlace/scripts/app.js
+++ marketPlace/scripts/app.js
@@ -1,7 +1,7 @@
-const main = document.querySelector('#principal-container')
+const main = document.querySelector('#principal-container');
 const btn_cart = document.querySelector('.btn-cart');
 const cart = document.querySelector('.cart');
-const productsCart = document.querySelector('.productsCart') 
+const productsCart = document.querySelector('.productsCart'); 
 
 const closeModal = document.querySelector('.close');
 const openModal = document.querySelector('.new-product');
@@ -18,10 +18,9 @@ cart_products= [];
 openModal.addEventListener('click', seeModal);
 closeModal.addEventListener('click', hiddenModal);
 window.addEventListener('click', closeModalcontainer);
-btn_cart.addEventListener('click', seeCart)
+btn_cart.addEventListener('click', seeCart);
 
 create_Cards()
-
 function seeCart(event){
   event.preventDefault();
   cart.classList.toggle('cart-container');
@@ -52,13 +51,12 @@ function closeModalcontainer(event){
 }
 
 function addProducts(){
-  let id = {id: products.length + 1}
+  let id = {id: products.length + 1};
   let name = {name: nameProduct.value};
   let price = {price: priceProduct.value};
   let description = {description: descriptionProduct.value};
-  let newProduct = Object.assign(id, name, price, description)
+  let newProduct = Object.assign(id, name, price, description);
   products.push(newProduct);
-  console.log(products)
 }
 
 function create_Cards() {
@@ -83,7 +81,7 @@ function create_Cards() {
     btn_card.addEventListener('click', addCart)
     
     title_card.textContent = product.name;
-    price_card.textContent = `$${price_product(product.price)} `;
+    price_card.textContent = `$${price_product(product.price)}`;
     img_card.src=product.img;
     description_card.textContent = product.description;
     
@@ -110,7 +108,6 @@ function addCart(event){
   cart_products.push(event.target.getAttribute('id'));
   showCart();
 }
-
 
 function showCart(){
   productsCart.innerHTML = ' ';
@@ -187,7 +184,7 @@ function subtractCart(event){
 
 function deleteProduct(event){
   cart_products = cart_products.filter(item => {
-    return  parseInt(event.target.getAttribute('id')) !== item.id 
+    return event.target.getAttribute('id') !== item;
   });
   showCart()
 }
